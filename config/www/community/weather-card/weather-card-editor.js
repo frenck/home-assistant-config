9f6c9dd501a68bf0d9595b4227407191ced1a365
Franck Nijhof <frenck@addons.community>
2019-12-24 15:56:43 +0100

    :arrow_up: Upgrade Weather Card to 1.4.1

--- config/www/community/weather-card/weather-card-editor.js
+++ config/www/community/weather-card/weather-card-editor.js
@@ -1,43 +1,26 @@
-if (!customElements.get("paper-input")) {
-  console.log("imported", "paper-input");
-  import("https://unpkg.com/@polymer/paper-input/paper-input.js?module");
-}
-if (!customElements.get("paper-toggle-button")) {
-  console.log("imported", "paper-toggle-button");
-  import(
-    "https://unpkg.com/@polymer/paper-toggle-button/paper-toggle-button.js?module"
-  );
-}
-if (!customElements.get("paper-dropdown-menu")) {
-  console.log("imported", "paper-dropdown-menu");
-  import(
-    "https://unpkg.com/@polymer/paper-dropdown-menu/paper-dropdown-menu.js?module"
-  );
-}
-if (!customElements.get("paper-listbox")) {
-  console.log("imported", "paper-listbox");
-  import("https://unpkg.com/@polymer/paper-listbox/paper-listbox.js?module");
-}
-if (!customElements.get("paper-item")) {
-  console.log("imported", "paper-item");
-  import("https://unpkg.com/@polymer/paper-item/paper-item.js?module");
-}
-
 const fireEvent = (node, type, detail, options) => {
   options = options || {};
   detail = detail === null || detail === undefined ? {} : detail;
   const event = new Event(type, {
     bubbles: options.bubbles === undefined ? true : options.bubbles,
     cancelable: Boolean(options.cancelable),
-    composed: options.composed === undefined ? true : options.composed
+    composed: options.composed === undefined ? true : options.composed,
   });
   event.detail = detail;
   node.dispatchEvent(event);
   return event;
 };
 
+if (
+  !customElements.get("ha-switch") &&
+  customElements.get("paper-toggle-button")
+) {
+  customElements.define("ha-switch", customElements.get("paper-toggle-button"));
+}
+
 const LitElement = Object.getPrototypeOf(customElements.get("hui-view"));
 const html = LitElement.prototype.html;
+const css = LitElement.prototype.css;
 
 export class WeatherCardEditor extends LitElement {
   setConfig(config) {
@@ -78,11 +61,10 @@ export class WeatherCardEditor extends LitElement {
     }
 
     const entities = Object.keys(this.hass.states).filter(
-      eid => eid.substr(0, eid.indexOf(".")) === "weather"
+      (eid) => eid.substr(0, eid.indexOf(".")) === "weather"
     );
 
     return html`
-      ${this.renderStyle()}
       <div class="card-config">
         <div>
           <paper-input
@@ -118,7 +100,7 @@ export class WeatherCardEditor extends LitElement {
                     slot="dropdown-content"
                     .selected="${entities.indexOf(this._entity)}"
                   >
-                    ${entities.map(entity => {
+                    ${entities.map((entity) => {
                       return html`
                         <paper-item>${entity}</paper-item>
                       `;
@@ -126,23 +108,23 @@ export class WeatherCardEditor extends LitElement {
                   </paper-listbox>
                 </paper-dropdown-menu>
               `}
-          <paper-toggle-button
+          <ha-switch
             .checked=${this._current}
             .configValue="${"current"}"
             @change="${this._valueChanged}"
-            >Show current</paper-toggle-button
+            >Show current</ha-switch
           >
-          <paper-toggle-button
+          <ha-switch
             .checked=${this._details}
             .configValue="${"details"}"
             @change="${this._valueChanged}"
-            >Show details</paper-toggle-button
+            >Show details</ha-switch
           >
-          <paper-toggle-button
+          <ha-switch
             .checked=${this._forecast}
             .configValue="${"forecast"}"
             @change="${this._valueChanged}"
-            >Show forecast</paper-toggle-button
+            >Show forecast</ha-switch
           >
         </div>
       </div>
@@ -164,27 +146,25 @@ export class WeatherCardEditor extends LitElement {
         this._config = {
           ...this._config,
           [target.configValue]:
-            target.checked !== undefined ? target.checked : target.value
+            target.checked !== undefined ? target.checked : target.value,
         };
       }
     }
     fireEvent(this, "config-changed", { config: this._config });
   }
 
-  renderStyle() {
-    return html`
-      <style>
-        paper-toggle-button {
-          padding-top: 16px;
-        }
-        .side-by-side {
-          display: flex;
-        }
-        .side-by-side > * {
-          flex: 1;
-          padding-right: 4px;
-        }
-      </style>
+  static get styles() {
+    return css`
+      ha-switch {
+        padding-top: 16px;
+      }
+      .side-by-side {
+        display: flex;
+      }
+      .side-by-side > * {
+        flex: 1;
+        padding-right: 4px;
+      }
     `;
   }
 }
