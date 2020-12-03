c8e344ad6a52ff5cbc0c46c5b64f3a230abbfa2e
Franck Nijhof <frenck@addons.community>
2020-12-03 11:17:39 +0100

    ⬆️ Upgrade HACS to 1.6.2

--- config/custom_components/hacs/operational/setup.py
+++ config/custom_components/hacs/operational/setup.py
@@ -1,7 +1,7 @@
 """Setup HACS."""
+from custom_components.hacs.enums import HacsStage
 from aiogithubapi import AIOGitHubAPIException, GitHub
 from homeassistant import config_entries
-from homeassistant.components.lovelace import system_health_info
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
 from homeassistant.const import __version__ as HAVERSION
 from homeassistant.exceptions import ConfigEntryNotReady
@@ -31,6 +31,11 @@ from custom_components.hacs.operational.setup_actions.websocket_api import (
     async_setup_hacs_websockt_api,
 )
 from custom_components.hacs.share import get_hacs
+
+try:
+    from homeassistant.components.lovelace import system_health_info
+except ImportError:
+    from homeassistant.components.lovelace.system_health import system_health_info
 
 
 async def _async_common_setup(hass):
@@ -107,15 +112,13 @@ async def async_startup_wrapper_for_yaml():
             .replace(" ", "_")
             .replace("-", "_")
         )
-        hacs.logger.info("Could not setup HACS, trying again in 15 min")
-        async_call_later(hacs.hass, 900, async_startup_wrapper_for_yaml())
+        hacs.log.info("Could not setup HACS, trying again in 15 min")
+        if int(hacs.system.ha_version.split(".")[1]) >= 117:
+            async_call_later(hacs.hass, 900, async_startup_wrapper_for_yaml)
+        else:
+            async_call_later(hacs.hass, 900, async_startup_wrapper_for_yaml())
         return
     hacs.system.disabled = False
-
-
-async def _wait_for_startup(event):
-    """Startup after the start event."""
-    await get_hacs().startup_tasks()
 
 
 async def async_hacs_startup():
@@ -123,17 +126,20 @@ async def async_hacs_startup():
     hacs = get_hacs()
 
     lovelace_info = await system_health_info(hacs.hass)
-    hacs.logger.debug(f"Configuration type: {hacs.configuration.config_type}")
+    hacs.log.debug(f"Configuration type: {hacs.configuration.config_type}")
     hacs.version = VERSION
-    hacs.logger.info(STARTUP)
+    hacs.log.info(STARTUP)
     hacs.system.config_path = hacs.hass.config.path()
     hacs.system.ha_version = HAVERSION
 
-    # Clear old storage files
-    await async_clear_storage()
-
     # Setup websocket API
     await async_setup_hacs_websockt_api()
+
+    # Set up frontend
+    await async_setup_frontend()
+
+    # Clear old storage files
+    await async_clear_storage()
 
     hacs.system.lovelace_mode = lovelace_info.get("mode", "yaml")
     hacs.system.disabled = False
@@ -144,12 +150,13 @@ async def async_hacs_startup():
 
     can_update = await get_fetch_updates_for(hacs.github)
     if can_update is None:
-        hacs.logger.critical("Your GitHub token is not valid")
+        hacs.log.critical("Your GitHub token is not valid")
         return False
-    elif can_update != 0:
-        hacs.logger.debug(f"Can update {can_update} repositories")
+
+    if can_update != 0:
+        hacs.log.debug(f"Can update {can_update} repositories")
     else:
-        hacs.logger.info(
+        hacs.log.info(
             "HACS is ratelimited, repository updates will resume when the limit is cleared, this can take up to 1 hour"
         )
         return False
@@ -177,21 +184,21 @@ async def async_hacs_startup():
                 await async_remove_entry(hacs.hass, hacs.configuration.config_entry)
         return False
 
-    # Add additional categories
-    hacs.common.categories = ELEMENT_TYPES
-    if hacs.configuration.appdaemon:
-        hacs.common.categories.append("appdaemon")
-    if hacs.configuration.netdaemon:
-        hacs.common.categories.append("netdaemon")
-
-    # Set up frontend
-    await async_setup_frontend()
-
     # Setup startup tasks
-    hacs.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STARTED, _wait_for_startup)
+    if hacs.status.new:
+        if int(hacs.system.ha_version.split(".")[1]) >= 117:
+            async_call_later(hacs.hass, 5, hacs.startup_tasks)
+        else:
+            async_call_later(hacs.hass, 5, hacs.startup_tasks())
+    else:
+        hacs.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STARTED, hacs.startup_tasks)
 
     # Set up sensor
     await async_add_sensor()
 
     # Mischief managed!
+    await hacs.async_set_stage(HacsStage.WAITING)
+    hacs.log.info(
+        "Setup complete, waiting for Home Assistant before startup tasks starts"
+    )
     return True
